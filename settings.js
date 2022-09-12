f5c27c4862e5af56eefa36cad941b832036ffc27
THARUUUUUU <113302624+THARUUUUUU@users.noreply.github.com>
2022-09-12 22:32:31 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,7 +36,7 @@ global.socialm = "GitHub: prabhasha2006" // github or insta name
 global.location = "Srilanka, Veyangoda, maligathanna" // location
 
 //bot body 
-global.alivepic = `https://i.ibb.co/cyyCjN7/20220803-081426.png` // alive picture \\ use "https://ibb.com" to upload photos. < 1 MB
+global.alivepic = `https://i.ibb.co/9h548Lz/Whats-App-Image-2022-09-12-at-10-25-51-PM.jpg` // alive picture \\ use "https://ibb.com" to upload photos. < 1 MB
 
 global.dragonchat = "false"  // chat bot  "true" / "false" (200 auto replies in this)
 global.callblock = "false" // call block "true" / "false" ("true"  to block callers)
